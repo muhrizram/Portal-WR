e028f2dccb6e566a60f2a5feb20e046209b9b470
Reynanur <reynanur2424@gmail.com>
2023-08-31 13:35:48 +0700

    cek validasi user role, company, backlog

--- src/Layouts/Backlog/shema/index.js
+++ src/Layouts/Backlog/shema/index.js
@@ -3,9 +3,9 @@ import * as yup from "yup";
 const textPlease = 'Please Input'
 
 const schemacompany = yup.object({
-    // taskName: yup.string().max(30).required(`${textPlease} Task Name`),
+    taskName: yup.string().max(30).required(`${textPlease} Task Name`),
     // taskDescription: yup.string().max(255).required(`${textPlease} Task Description`),
-    // estimationTime: yup.number().required(`${textPlease} Estimation Time`),
+    estimationTime: yup.number().required(`${textPlease} Estimation Time`),
     // statusBacklog: yup.string().required(`${textPlease} Status Backlog`),
   })
 
