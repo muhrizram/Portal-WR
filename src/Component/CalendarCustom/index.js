a72c0d0c8ba6452a9bf1a47d3a71a35f672cb2a5
Muhammad Rizki Ramadhan <muhrizram19@gmail.com>
2024-04-30 13:56:34 +0700

    Change FullCalendar to react BigCalendar

--- src/Component/CalendarCustom/index.js
+++ src/Component/CalendarCustom/index.js
@@ -27,6 +27,10 @@ import "./calender.css";
 import HolidayDialog from "../DialogHoliday";
 import DateRangeCalendar from "../../Component/DateRangeCalendar";
 import { styled } from "@mui/system";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "moment/locale/en-gb";
+const localizer = momentLocalizer(moment);
 
 export default function BigCalendar({
   setOnClick,
@@ -725,6 +729,214 @@ export default function BigCalendar({
     }
   };
 
+  const createAllEvents = () => {
+    const today = moment().startOf("day");
+    let mappedEvents = []; // Menginisialisasi mappedEvents sebagai array kosong
+
+    events
+      .filter((event) => moment(event.tanggal).isSameOrBefore(today, "day"))
+      .forEach((event) => {
+        let title = "";
+
+        if (event.presenceName == null && event.holiday == false) {
+          title = "Attendance";
+        } else {
+          title = event.presenceName;
+        }
+
+        const attendance = {
+          ...event,
+          paramAttendance: true,
+          title: title,
+          start: event.tanggal,
+          end: event.tanggal,
+        };
+        mappedEvents.push(attendance);
+
+        if (event.overtime == true) {
+          title = "View Overtime";
+        } else {
+          title = "Overtime";
+        }
+
+        const overtime = {
+          ...event,
+          paramOvertime: true,
+          title: title,
+          start: event.tanggal,
+          end: event.tanggal,
+        };
+        mappedEvents.push(overtime);
+
+        if (event.holiday == true) {
+          title = "Holiday";
+        } else {
+          title = "Task";
+        }
+        const task = {
+          ...event,
+          paramTask: true,
+          title: title,
+          start: event.tanggal,
+          end: event.tanggal,
+        };
+        mappedEvents.push(task);
+      });
+
+    return mappedEvents;
+  };
+
+  const components = {
+    event: ({ event }) => {
+      let clickHere = null;
+      const isNotCheckIn = event.title === "Attendance";
+      const isCheckIn = event.title === "Hadir";
+      const isCheckInButNotPresent =
+        event.presenceName !== "Hadir" && event.paramAttendance;
+      const isHoliday = event.title === "Holiday";
+      const isViewOvertime = event.title === "View Overtime";
+      const isOvertime =
+        (event.title === "Overtime" && event.presenceName === "Hadir") ||
+        event.holiday === true;
+      const isTaskNotFilled =
+        !event.task && event.title === "Task" && event.presenceName === "Hadir";
+      const isTaskFilled = event.task && event.title !== "Hadir";
+
+      if (isNotCheckIn) {
+        clickHere = () => {
+          setOnClick(event);
+        };
+      } else if (isCheckInButNotPresent) {
+        clickHere = () => {
+          setWrIdDetail(event.workingReportTaskId);
+          setIsViewAttendance(true);
+        };
+      } else if (isHoliday) {
+        clickHere = () => {
+          setdescHoliday(event.descHoliday);
+          setTanggalHoliday(event.tanggal);
+          setDialogOpenHoliday(true);
+        };
+      } else if (isViewOvertime) {
+        clickHere = () => {
+          setId(event.workingReportOvertimeId);
+          setWrIdDetail(event.workingReportOvertimeId);
+          setIsViewOvertime(true);
+          setonOtherUser(true);
+        };
+      } else if (isOvertime) {
+        clickHere = () => {
+          setOpenOvertime(true);
+          setId(event.workingReportOvertimeId);
+          setWrDate(event.tanggal);
+        };
+      } else if (isTaskNotFilled) {
+        clickHere = () => {
+          setOpenTask(true);
+          setId({
+            workingReportTaskId: event.workingReportTaskId,
+            absenceId: event.absenceId,
+          });
+        };
+      } else if (isTaskFilled) {
+        clickHere = () => {
+          setId({
+            workingReportTaskId: event.workingReportTaskId,
+            absenceId: event.absenceId,
+          });
+          setWrIdDetail(event.workingReportTaskId);
+          setIsViewTask(true);
+        };
+      }
+
+      return (
+        <Typography
+          sx={{ color: "white" }} // Menghilangkan background biru
+          onClick={clickHere}
+        >
+          {event.title}
+        </Typography>
+      );
+    },
+  };
+
+  const allEvents = createAllEvents();
+
+  const filterDate = (date, { startDate, endDate }) => {
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const startDay = new Date(startDate).getDate();
+    const startMonth = new Date(startDate).getMonth();
+    const startYear = new Date(startDate).getFullYear();
+    const endDay = new Date(endDate).getDate();
+    const endMonth = new Date(endDate).getMonth();
+    const endYear = new Date(endDate).getFullYear();
+    const isWithinRange =
+      (year > startYear ||
+        (year === startYear && month > startMonth) ||
+        (year === startYear && month === startMonth && day >= startDay)) &&
+      (year < endYear ||
+        (year === endYear && month < endMonth) ||
+        (year === endYear && month === endMonth && day <= endDay));
+
+    if (startDate !== null && isWithinRange) {
+      if (date.getDay() === 0 || date.getDay() === 6) {
+        return { className: "selected-weekend" };
+      }
+      return { className: "date-selected" };
+    }
+    if (date.getDay() === 0 || date.getDay() === 6) {
+      return { className: "date-weekend" };
+    }
+
+    return {};
+  };
+
+  const eventStyle = (event, start, end, isSelected) => {
+    let backgroundColor = "transparent";
+    let opacity = 0.2;
+    if (event.title === "Hadir") {
+      backgroundColor = "green";
+    } else if (event.title === "Sakit") {
+      backgroundColor = "red";
+    } else if (event.title === "Cuti") {
+      backgroundColor = "orange";
+    } else if (event.title === "Izin") {
+      backgroundColor = "yellow";
+    } else if (event.title === "Overtime") {
+      backgroundColor = "purple";
+      if (event.presenceName === "Hadir" || event.holiday === true) {
+        opacity = 1;
+      }
+    } else if (event.title === "View Overtime") {
+      backgroundColor = "black";
+      opacity = 1;
+    } else if (event.title === "Attendance") {
+      backgroundColor = "grey";
+      opacity = 1;
+    } else if (event.title === "Task") {
+      backgroundColor = "yellow";
+      if (event.presenceName === "Hadir") {
+        opacity = 1;
+      }
+    } else if (event.title === "Holiday") {
+      backgroundColor = "red";
+      opacity = 1;
+    }
+    const style = {
+      backgroundColor: backgroundColor,
+      borderRadius: "0px",
+      opacity: opacity,
+      border: "none",
+      color: "black",
+      display: "block",
+    };
+    return {
+      style,
+    };
+  };
+
   return (
     <Grid>
       {!changeCurrentMonth && (
@@ -764,43 +976,18 @@ export default function BigCalendar({
         setStartDateCall={setStartDate}
         setWeekendDates={setWeekendDates}
       />
-      <FullCalendar
-        plugins={[
-          dayGridPlugin,
-          timeGridPlugin,
-          interactionPlugin,
-          scrollGridPlugin,
-        ]}
-        // initialView={"dayGridMonth"}
-        firstDay={1}
-        key={activeMonth.getTime()}
-        initialDate={activeMonth}
-        dayCellContent={(info, create) => renderCalendar(info)}
-        height={1400}
-        dayMinWidth={200}
-        selectable={true}
-        eventContent={renderEventContent}
-        headerToolbar={{
-          start: "",
-          center: "",
-          end: "",
-        }}
-        events={[
-          { events },
-          {
-            start: StartDate,
-            end: finalDateCalendar,
-            display: "background",
-            color: "#b0d8f7",
-          },
-          ...(Array.isArray(weekendDates)
-            ? weekendDates.map((date) => ({
-                start: date,
-                display: "background",
-                color: "red",
-              }))
-            : []),
-        ]}
+      <Calendar
+        localizer={localizer}
+        views={["month"]}
+        events={allEvents}
+        components={components}
+        style={{ height: 1100 }}
+        date={activeMonth}
+        toolbar={false}
+        dayPropGetter={(date) =>
+          filterDate(date, { startDate: StartDate, endDate: EndDate })
+        }
+        eventPropGetter={eventStyle}
       />
       <Dialog
         fullWidth={fullWidth}
