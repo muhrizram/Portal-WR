7c7428928cc1d70cd085200421b4bc4dd2beb3bd
Muhammad Rizki Ramadhan <muhrizram19@gmail.com>
2024-05-16 23:16:32 +0700

    refactor: Add projectSchema for validation in global module

---
+++ src/global/projectSchema.js
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import moment from "moment";
+
+export const projectSchema = z.object({
+  projects: z.array(
+    z.object({
+      projectId: z.string().min(1, {
+        message: "Project ID is required",
+      }),
+    })
+  ),
+  tasks: z.array(
+    z.array(
+      z.object({
+        taskName: z.string().min(1, {
+          message: "Task Name is required",
+        }),
+        statusTaskId: z.string().min(1, {
+          message: "Status Task ID is required",
+        }),
+        duration: z.string().min(1, {
+          message: "Duration is required",
+        }),
+      })
+    )
+  ),
+});
