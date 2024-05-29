737affa9ab5da7abf129c47d21df625d1bd180b2
Muhammad Rizki Ramadhan <muhrizram19@gmail.com>
2024-05-29 16:33:24 +0700

    Fix startTime and endTime cannot be filled between working hours

--- src/global/overtimeSchema.js
+++ src/global/overtimeSchema.js
@@ -12,6 +12,15 @@ const timeSchema = z
           return moment(value, "HH:mm:ss").isValid();
         },
         { message: "Start Time is invalid" }
+      )
+      .refine(
+        (value) => {
+          const time = moment(value, "HH:mm:ss");
+          const startRange = moment("08:00:00", "HH:mm:ss");
+          const endRange = moment("17:00:00", "HH:mm:ss");
+          return !time.isBetween(startRange, endRange, null, "[)");
+        },
+        { message: "Start Time cannot be between 08:00 and 17:00" }
       ),
     endTime: z
       .string()
@@ -21,6 +30,15 @@ const timeSchema = z
           return moment(value, "HH:mm:ss").isValid();
         },
         { message: "End Time is invalid" }
+      )
+      .refine(
+        (value) => {
+          const time = moment(value, "HH:mm:ss");
+          const startRange = moment("08:00:00", "HH:mm:ss");
+          const endRange = moment("17:00:00", "HH:mm:ss");
+          return !time.isBetween(startRange, endRange, null, "[)");
+        },
+        { message: "End Time cannot be between 08:00 and 17:00" }
       ),
   })
   .refine((data) => data.startTime <= data.endTime, {
